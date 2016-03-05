926805dd5f0e1b7809836771c5a24a595d2906d7
jkrvivian <jkrvivian@gmail.com>
2016-03-06 00:54:18 +0800

    move to new structure

--- main.c
+++ main.c
@@ -104,15 +104,17 @@ int main(int argc, char *argv[])
     cpu_time2 = diff_in_second(start, end);
 
     FILE *output;
-#if defined(OPT)
+#if defined (OPT)
     output = fopen("opt.txt", "a");
+#else
+    output = fopen("orig.txt", "a");
 #endif
 
 #if defined (HASH)
     output = fopen("hash1.txt", "a");
-#else
-    output = fopen("orig.txt", "a");
 #endif
+
+
     fprintf(output, "append() findName() %lf %lf\n", cpu_time1, cpu_time2);
     fclose(output);
 
